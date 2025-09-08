f9bb04f3de0c8178e8e0cf920716f367144ed068
Ryan Omasta <git@ryand.ca>
2025-09-08 11:32:55 -0600

    feat: update nix versions

--- flake/nix-versions.nix
+++ flake/nix-versions.nix
@@ -36,9 +36,9 @@ in
       attic.nix-versions = {
         versions = {
           default = pkgs.nix;
-          "2.24" = pkgs.nixVersions.nix_2_24;
-          "2.26" = pkgs.nixVersions.nix_2_26;
           "2.28" = pkgs.nixVersions.nix_2_28;
+          "2.29" = pkgs.nixVersions.nix_2_29;
+          "2.30" = pkgs.nixVersions.nix_2_30;
         };
 
         manifestFile = let
